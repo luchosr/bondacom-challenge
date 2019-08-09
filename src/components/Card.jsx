b62e057f2d6288d2a4d7a38b45c7db1da36dd0a7
Luciano <luchosr@gmail.com>
2019-08-09 17:38:44 -0300

    add card structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "../App.css";
 
-const Card = () => {
+const Card = ({ tag, title, partner, expDate, category, imgUrl }) => {
   return (
     <div className="card-container">
-      <div className="card-container-decription">
+      <div className="card-container-description">
         <h2>0%</h2>
-        <h3>Card Title | dos lineas como máximo</h3>
+        <h5>Card Title | dos lineas como máximo</h5>
+        <div className="partner-text">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOh9BacywpRba8Dz8-dUjyABTIAwNkd8lRajAPZiAMSj4tdd2Hbw"
+            alt=""
+            className="partner-image"
+          />
+          <h6> Nombre partner</h6>
+
+          <h6>fecha vencimiento y aca iría el reminder</h6>
+        </div>
+      </div>
+      <div className="card-container-image">
+        <img
+          src="https://picsum.photos/id/870/100/100?grayscale&blur=2"
+          alt=""
+        />
       </div>
     </div>
   );
